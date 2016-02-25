879dca5ecc1a10d6995746901613ac43ebf3fc45
albrodpul <albrodpul@gmail.com>
2016-02-25 18:40:36 +0100

    prueba

--- index2.js
+++ index2.js
@@ -1,11 +1,23 @@
-var contacts=["pablo","pepe","luis"];
-
-//cabecera
+﻿//cabecera
 //cada contacto en una línea
 //pie
-var i=0
-console.log("Contacts:");
-for(i=0;i<contacts.length;i++){
-	console.log(i+1+".- " +contacts[i]);
-}
-console.log("---------------");
+var express=require("express");
+var fs=require("fs");
+var contacts=[];
+var app=express();
+
+
+app.get("/about/",(req,res) => {
+	fs.readFile('contacts.json','utf8',(err,content) => {
+		console.log("Data read");
+		contacts=JSON.parse(content);
+		res.write("<html><body>Contacts:<ul>");
+
+		contacts.forEach((contact) =>{
+			res.write("<li>"+contact.name+" ("+contact.phone+")</li>");
+		});
+		res.write("</ul>---------------</body></html>");
+		res.end();
+	});
+});
+app.listen(process.env.PORT);
