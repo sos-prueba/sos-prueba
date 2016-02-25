23b6790efd54f9ffbad9a29c3e39c6faa1be2dce
albrodpul <albrodpul@gmail.com>
2016-02-25 18:35:31 +0100

    test

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-var cool = require("cool-ascii-faces");
-var express = require("express");
+/*var contacts=[
+			{ name: "pablo", phone: "123456789"},
+			{ name: "alberto", phone: "987654321"}
+			];
+*/
+//Cargar el módulo
+var express=require("express");
+var fs=require("fs");
+var contacts=[];
+var app=express();
 
-var app = express();
 
-app.get("/", function (req,res){
-	console.log("New request arrived!");
-	res.send(cool());
+app.get("/",(req,res) => {
+	fs.readFile('contacts.json','utf8',(err,content) => {
+		console.log("Data read");
+		contacts=JSON.parse(content);
+		res.write("<html><body>Contacts:<ul>");
 
+		contacts.forEach((contact) =>{
+			res.write("<li>"+contact.name+" ("+contact.phone+")</li>");
+		});
+		res.write("</ul>---------------</body></html>");
+		res.end();
+	});
 });
+app.listen(process.env.PORT);
 
-app.listen(5000);
+//Leer de forma síncrona
+//var data=fs.readFileSync('contacts.json','utf8');
+//console.log(data);
+
+//Leer de forma asíncrona
+/*fs.readFile('contacts.json','utf8',(err,data) => {
+	var contacts=JSON.parse(data);
+	console.log("Contacts:");
+
+	contacts.forEach((contact) =>{
+		console.log(" - "+contact.name+" ("+contact.phone+")");
+	});
+
+	console.log("---------------");
+});*/
+
+//Parsea los datos
+//var contacts=JSON.parse(data);
+
+//Bucle for que hace la misma función
+/*
+for(var i=0;i<contacts.length;i++){
+	var contact=contacts[i];
+	imprimeContacto(contact);
+}
+console.log("---------------");
+*/
+/*function imprimeContacto(contact){
+	console.log(i+1+".- "+contact.name+" ("+contact.phone+")");
+}*/
+
+/*---------------------------------------------------*/
+//Llama a la función e itera sin bucle
+//contacts.forEach(imprimeContacto);
+
+
+/*---------------------------------------------------*/
+//Función lambda (llamada dentro de la declaración de la función)
+/*contacts.forEach(function(contact){
+	console.log(i+1+".- "+contact.name+" ("+contact.phone+")");
+});*/
+
+
+/*---------------------------------------------------*/
+//Misma función lambda, pero sin declararlo como función
+/*contacts.forEach((contact) =>{
+	console.log(" - "+contact.name+" ("+contact.phone+")");
+});*/
---
+++ index2.js
@@ -0,0 +1,11 @@
+var contacts=["pablo","pepe","luis"];
+
+//cabecera
+//cada contacto en una línea
+//pie
+var i=0
+console.log("Contacts:");
+for(i=0;i<contacts.length;i++){
+	console.log(i+1+".- " +contacts[i]);
+}
+console.log("---------------");
