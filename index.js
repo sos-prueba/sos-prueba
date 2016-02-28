c565f91db2ec890015496249c6ccd8d25165c194
albrodpul <albrodpul@gmail.com>
2016-02-28 11:46:21 +0100

    test

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-/*var contacts=[
-			{ name: "pablo", phone: "123456789"},
-			{ name: "alberto", phone: "987654321"}
-			];
-*/
-//Cargar el módulo
 var express=require("express");
 var fs=require("fs");
-var members=[];
 var app=express();
 
 app.get("/about/spain-births",(req,res)=>{
-	fs.readFile('spain-births.json','utf8',(err,content)=>{
+	fs.readFile('spain-births.json','utf-8',(err,content)=>{
 		console.log("Data read");
 		births=JSON.parse(content);
-		res.write('<html><body>It will display data from spanish births, by regions and years,<br /> making difference between men and women, showing the total number in the last column.<br />');
-		res.write("Data:<ul>");
+		res.write('<html><body><h3>Spanish births </h3><br />');
+		res.write('It will display data about spanish births, by regions and years. <br />Making difference between men and women, showing the total number of births in the last column, as following: <br />');		
+		res.write("Data example:<ul>");
 		res.write("<li>region, year => men, women, total birth</li>")
 		births.forEach((birth) =>{
 			res.write("<li>"+birth.region+", "+birth.year+" => "+birth.men+", "+birth.women+", "+birth.totalbirth+"</li>");
@@ -25,14 +19,53 @@ app.get("/about/spain-births",(req,res)=>{
 	
 });
 
+app.get("/about/population-growth",(req,res)=>{
+	fs.readFile('population-growth.json','utf-8',(err,content)=>{
+		console.log("Data read");
+		pops=JSON.parse(content);
+		res.write('<html><h2>Population growth</h2>');
+		res.write('<body>This page shows data about population growth in Spain in the lastest years. <br /> Data are classified acording to differents parameters, such as: <br /><ul>');
+		res.write('<li>Region</li>');
+		res.write('<li>Year</li>');
+		res.write('<li>Age</li>');
+		res.write('<li>Men</li>');
+		res.write('<li>Women</li>');
+		res.write('<li>Total population</li>');
+		res.write("</ul>Data example:<ul>");
+		res.write("<li>region, year => men, women, total population</li>")
+		pops.forEach((pop) =>{
+			res.write("<li>"+pop.region+", "+pop.year+" => "+pop.men+", "+pop.women+", "+pop.totalpopulation+"</li>");
+		});
+		res.write("</body></html>");
+		res.end();
+	});
+	
+});
+
+app.get("/about/mort-sickness",(req,res)=>{
+	fs.readFile('mort-sickness.json', 'utf8', (err, content)=>{
+		console.log("This is my data source");
+		sic = JSON.parse(content);
+		res.write('<html><h1>Mort sickness</h1>');
+		res.write('<body>My data source is about mortality sexually transmited disease');
+		sic.forEach((sickness)=>{
+			res.write(" - ", + sickness.region + "  " + sickness.sickness + "  " + sickness.year + "  " 
+				+ sickness.mortalityinmen  + "  " + sickness.mortalityinwomen + "  "
+				  + sickness.totalmortality);
+		});
+		res.write("</body></html>");
+		res.end();
+	});
+});
+
 app.get('/about',(req,res) => {
-	fs.readFile('members.json','utf8',(err,content) => {
+	fs.readFile('members.json','utf-8',(err,content) => {
 		console.log("Data read");
 		members=JSON.parse(content);
 		res.write("<html><body>Group members:<ul>");
 
 		members.forEach((member) =>{
-			res.write("<li>"+member.name+" =>"+member.source+"</li>");
+			res.write("<li>"+member.name+" => <a href=https://sos-2016-03.herokuapp.com/about/"+member.source+">"+member.source+"</a></li>");
 		});
 		res.write("</ul>Our sources of information are aimed for analyzing the relationship between the number of births<br />over the years in the regions of Spain, along with the number of deaths<br /> due to different types of disease and population growth in our country .</body></html>");
 		res.end();
@@ -40,51 +73,3 @@ app.get('/about',(req,res) => {
 });
 app.listen(process.env.PORT);
 
-//Leer de forma síncrona
-//var data=fs.readFileSync('contacts.json','utf8');
-//console.log(data);
-
-//Leer de forma asíncrona
-/*fs.readFile('contacts.json','utf8',(err,data) => {
-	var contacts=JSON.parse(data);
-	console.log("Contacts:");
-
-	contacts.forEach((contact) =>{
-		console.log(" - "+contact.name+" ("+contact.phone+")");
-	});
-
-	console.log("---------------");
-});*/
-
-//Parsea los datos
-//var contacts=JSON.parse(data);
-
-//Bucle for que hace la misma función
-/*
-for(var i=0;i<contacts.length;i++){
-	var contact=contacts[i];
-	imprimeContacto(contact);
-}
-console.log("---------------");
-*/
-/*function imprimeContacto(contact){
-	console.log(i+1+".- "+contact.name+" ("+contact.phone+")");
-}*/
-
-/*---------------------------------------------------*/
-//Llama a la función e itera sin bucle
-//contacts.forEach(imprimeContacto);
-
-
-/*---------------------------------------------------*/
-//Función lambda (llamada dentro de la declaración de la función)
-/*contacts.forEach(function(contact){
-	console.log(i+1+".- "+contact.name+" ("+contact.phone+")");
-});*/
-
-
-/*---------------------------------------------------*/
-//Misma función lambda, pero sin declararlo como función
-/*contacts.forEach((contact) =>{
-	console.log(" - "+contact.name+" ("+contact.phone+")");
-});*/
